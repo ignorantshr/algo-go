55826760c06a5ac5b38b11c027738ac22f63c715
ignorantshr <shrshraa@outlook.com>
2023-10-30 11:38:12 +0800

    链表-双链表

--- datastruct/线性表/链表-双链表.c
+++ datastruct/线性表/链表-双链表.c
@@ -28,7 +28,7 @@ DLinkList initHeadDList(int len, int vals[]) {
 }
 
 // 不带头结点的双链表
-DLinkList initHeadDList(int len, int vals[]) {
+DLinkList initDList(int len, int vals[]) {
     DLinkList l;
     DNode* tail = l;
 
@@ -45,6 +45,14 @@ DLinkList initHeadDList(int len, int vals[]) {
     }
     tail->next = NULL;
     return l;
+}
+
+void DestroyDlist(DLinkList l) {
+    while (l != NULL) {
+        DNode* next = l->next;
+        free(l);
+        l = next;
+    }
 }
 
 bool insert_before(DNode* cur, DNode* n) {
@@ -73,4 +81,68 @@ bool insert_after(DNode* cur, DNode* n) {
     cur->next = n;
     n->pre = cur;
     return true;
+}
+
+// 删除带头结点的链表
+bool deleteNodeHead(DNode* n) {
+    if (n == NULL) {
+        return false;
+    }
+
+    n->pre->next = n->next;
+    if (n->next != NULL) {
+        n->next->pre = n->pre;
+    }
+    free(n);
+    return true;
+}
+
+// 删除不带头结点的链表
+bool deleteNode(DNode* n) {
+    if (n == NULL) {
+        return false;
+    }
+
+    if (n->pre != NULL) {
+        n->pre->next = n->next;
+    }
+    if (n->next != NULL) {
+        n->next->pre = n->pre;
+    }
+    free(n);
+    return true;
+}
+
+void printlist(DLinkList l) {
+    printf("[");
+    for (DNode* n = l; n != NULL; n = n->next) {
+        printf("%d ", n->data);
+    }
+    printf("]\n");
+}
+
+int main(int argc, char const* argv[]) {
+    int vals[] = {1, 2, 3};
+    DLinkList hl1 = initHeadDList(3, vals);
+    printlist(hl1);
+    DNode* n = (DNode*)malloc(sizeof(DNode));
+    n->data = 4;
+    n->next = NULL;
+    // insert_after(hl1, n);
+    // deleteNodeHead(hl1->next);
+    deleteNodeHead(hl1->next->next->next);
+    printlist(hl1);
+
+    DLinkList l2 = initDList(3, vals);
+    printlist(l2);
+    // insert_before(l2->next, n);
+    insert_before(l2, n);
+    l2 = n;
+    printlist(l2);
+    // deleteNode(l2->next);
+    // n = l2->next;
+    // deleteNode(l2);
+    // l2 = n;
+    deleteNode(l2->next->next->next);
+    printlist(l2);
 }
